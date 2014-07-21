3c97e690c07081795c8cf6eb81109c7c80c84c0e
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-07-21 16:06:25 +0200

    Preparing release 1.0b1

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0a1'
+version = '1.0b1'
+
+README = open("README.rst").read()
+HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
 
 setup(name='transparencia.theme',
       version=version,
       description='',
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
+      long_description=README + "\n" + HISTORY,
       classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='theme genweb plone',
       author='UPCnet Plone Team',
       author_email='plone.team@upcnet.es',
